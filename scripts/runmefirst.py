dfcb090c68ee130956cb9813a0472b0305552c9c
Tom Lamphier <thomaslamphier@gmail.com>
2019-10-13 12:28:41 -0400

    more cleanup

---
+++ scripts/runmefirst.py
@@ -0,0 +1,18 @@
+#
+# runmefirst.py - quick check to see if all the requires python packages
+#                 are installed
+#
+#                 python will print an error message if a module is not found
+#
+# usage:  python3 runmefirst.py
+#
+import os
+import sys
+import pymongo
+import json
+import area
+import numpy
+import UliEngineering.Math.Coordinates
+import math
+import csv
+import decimal
